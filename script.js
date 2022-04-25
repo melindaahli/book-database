cbcdac8d426b57eac0e9ff935b6fa7e7eb8582c2
Glitch (responsible-forest-soapwort) <none>
2022-04-25 23:57:50 +0000

    🗽💑 Checkpoint ./style.css:37657341/20 ./index.html:37657332/554:37657341/386 ./script.js:37657332/192:37657341/629

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-let URL = `http://www.omdbapi.com/?apikey=8e9ded79&y=${inputYear}&s=${inputTerm}`
+$("#searchButton").click(function(){
+  
+  let inputYear = $("#yearBar").val();
+  let inputTerm = $("#searchBar").val();
+  
+  let URL = `http://www.omdbapi.com/?apikey=8e9ded79&y=${inputYear}&s=${inputTerm}`;
 
-let inputYear = 2016;
-let inputTerm = "";
+  console.log(inputYear);
+  console.log(inputTerm);
+  
+  fetch(URL)
+        .then(function(response) {
+            return response.json();
+  })
+        .then(function(data) {
+            console.log(data);
+        });
+});
