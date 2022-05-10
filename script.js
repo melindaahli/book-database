a7ee39820451d7624324ea9689fa16bd03227128
Glitch (responsible-forest-soapwort) <none>
2022-05-10 00:05:59 +0000

    🏞🐇 Checkpoint ./index.html:37657341/39 ./style.css:37657341/102 ./script.js:37657341/1808 ./reviews.html:37657341/834

--- script.js
+++ script.js
@@ -3,26 +3,20 @@ $("#searchButton").click(function () {
   let inputTerm = $("#searchBar").val();
 
   let URL = `https://www.googleapis.com/books/v1/volumes?q=${inputTerm}+inauthor:${inputAuthor}`;
-
   console.log(URL);
+
+  $(".bookresults").html("");
 
   fetch(URL)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-      console.log(data.items[0].volumeInfo.title);
       data.items.forEach((book) => {
-        console.log(book);
-        /*
-        $(".bookResults").append("<div class='volume'>");
-        $(".bookResults").append(`<img src=${book.volumeInfo.imageLinks.thumbnail}/>`);
-        $(".bookResults").append(`<p>${book.volumeInfo.title}</p>`);
-        $(".bookResults").append("</div>");
-        */
         $(".bookResults").append(`<div class='volume'>
-        <img src=${book.volumeInfo.imageLinks.thumbnail}/ >
+        <a href="">
+        <img src=${book.volumeInfo.imageLinks.thumbnail}/>
+        </a>
         <p class="sans-serif">${book.volumeInfo.title}</p>
         </div>
         `);
